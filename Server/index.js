582b5927730c4a7367b105677c94d43b751b7b9d
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2025-02-13 10:48:52 +0530

    updates

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : "https://gmail-clone-ggn9.onrender.com",
     credentials : true
 }
 app.use(cors(corsOptions))
